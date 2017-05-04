5e2a459525afb9b17a45d244862fc632f93ca059
ThreeTen22 <gtumen@gmail.com>
2017-05-03 21:43:29 -0500

    changed path

--- objects/crew/outfitpanescript.lua
+++ objects/crew/outfitpanescript.lua
@@ -1,12 +1,40 @@
+require "/scripts/util.lua"
+require "/scripts/interp.lua"
+require "/scripts/messageutil.lua"
 
 function init()
 	return
 end
 
 function update(dt)
+	promises:update()
 	return 
 end
 
-function grid(...)
-	dLogJson({...}, "grid", true)
+function checkForItemChanges(itemBag, contentsChanged)
+    for i = 1, self.slotCount do
+      if not compare(self.equipBagStorage[i], itemBag[i]) then
+        if itemBag[i] ~= nil and (not inCorrectSlot(i, itemBag[i])) then
+          if promises:empty() then
+            promises:add(world.sendEntityMessage(pane.containerEntityId(), "removeItemAt", i), player.giveItem(itemBag[i]))
+          end
+        end
+        if not (self.items.override) then
+          self.items.override = jarray()
+          self.items.override = npcUtil.buildItemOverrideTable(self.items.override)
+        end
+        local insertPosition = self.items.override[1][2][1]
+        --Add items to override item slot so they update visually.
+        setItemOverride(self.equipSlot[i],insertPosition, itemBag[i])
+        contentsChanged = true
+      end
+    end
+
+    if contentsChanged then 
+      if npcUtil.isContainerEmpty(itemBag) then
+        self.items.override = nil
+      end
+    end
+    self.equipBagStorage = widget.itemGridItems("itemGrid") 
+    return contentsChanged
 end
